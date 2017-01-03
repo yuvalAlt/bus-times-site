268b86b7644e386776e9de9e3105b590ba4010ca
yuvalAlt <yuvals@my.yorku.ca>
2017-01-03 16:19:43 -0500

    init

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function () {
+    var yrtBusOps = $('yrt_routes_options');
+    var yrtTimeOps = $('yrt_time_options');
+
+    
+
+})
+
+function getNumOps() {
+
+}
+
+function getRoutesYRT() {
+    
+}
+
+function addOpsYRT() {
+
+}
