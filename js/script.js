4e636caafa78f6c8312c2fda17f60905eca47603
yuvalAlt <yuvals@my.yorku.ca>
2017-01-06 13:19:39 -0500

    Added some dummy options for the routes

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 $(function () {
-    var yrtBusOps = $('yrt_routes_options');
+    var yrtBusOps = document.getElementById("yrt_routes_options");
     var yrtTimeOps = $('yrt_time_options');
 
-    
+    var busLine = document.createElement("option");
+    var optTemp = document.createTextNode("The First Option of Its Kind");
+    busLine.appendChild(optTemp);
+    yrtBusOps.appendChild(busLine);
 
-})
-
-function getNumOps() {
-
+});
+// Is there another YRT route
+function hasNextYRT(i) {
+    i = i + 1;
+    return i < 5;
 }
-
-function getRoutesYRT() {
-    
-}
-
-function addOpsYRT() {
-
+// Get the next YRT route info
+function getNextYRTNum(i) {
+    return i;
 }
